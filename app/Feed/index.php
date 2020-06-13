025dc9e4db8d07c2cf28d8927a4cda5e610ccdef
oleg <soe190715@gmail.com>
2020-06-13 16:10:24 +0700

    index feed

--- app/Feed/index.php
+++ app/Feed/index.php
@@ -1,11 +1,24 @@
 <?php
 declare(strict_types=1);
+ini_set("memory_limit",'1G');
 
 use App\Feed\Builder\Ebay\Ebay;
+use App\Feed\Faker;
 use App\Feed\MakeFile\MakeCSV;
 
-$data = \App\Feed\Faker::makeArray(20, 100000);
+$data = Faker::makeArray(20, 1000000);
+
+$start = \microtime(true);
 
 $ebay = new Ebay($data);
+$file = new MakeCSV($ebay->getData());
 
-var_dump($ebay->getData());
+$mem = \memory_get_peak_usage(true) / 1024 / 1024;
+$end = \microtime(true) - $start;
+echo 'Build:  time: ' . \round($end, 2) . ' mem: ' . \round($mem, 2) . "\n";
+
+$file->make();
+
+$mem = \memory_get_peak_usage(true) / 1024 / 1024;
+$end = \microtime(true) - $start;
+echo 'File:  time: ' . \round($end, 2) . ' mem: ' . \round($mem, 2) . "\n";
